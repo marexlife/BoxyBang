76c1fa223b976570c59b25a1abdf52c107f53dd2
marcel <marcel.hajek@gmail.com>
2026-03-16 16:05:21 +0100

    ...

---
+++ CMake/DefaultFlags.cmake
@@ -0,0 +1,20 @@
+set(BOXY_COMMON_FLAGS
+    -fno-rtti
+    -O3
+)
+
+if(MSVC)
+    set(BOXY_COMMON_FLAGS
+        ${BOXY_COMMON_FLAGS}
+        /W4
+    )
+else()
+    set(BOXY_COMMON_FLAGS
+        ${BOXY_COMMON_FLAGS}
+        -Wall
+        -Wextra
+        -Wpedantic
+        -Wconversion
+        -Werror
+    )
+endif()
